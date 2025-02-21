95d2d6cd9ef60a940f0c57074fc3f75907b4206b
Sushanth-Personal <sushanthp.calicut@gmail.com>
2025-02-21 15:29:57 +0530

    Add: Basic setup and LandingPage First Banner added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+  } from "react-router-dom";
+  import LandingPage from "./pages/LandingPage/LandingPage";
+  
+  function App() {
+
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </Router>
+    );
+  }
+  
+  export default App;
+  
